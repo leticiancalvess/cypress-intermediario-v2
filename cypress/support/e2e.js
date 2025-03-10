c954d053cda9efbaf84e814dfa37e08b975fee49
leticiancalvess <leticiancalves1@gmail.com>
2025-03-09 21:27:21 -0300

    visual feedback for api testing

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -14,6 +14,7 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
+import 'cypress-plugin-api'
 import './gui_commands'
 import './api_commands'
 
